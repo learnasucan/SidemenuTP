1f2e11f27b5c9f2f4b805470cd0c17d8e58306bc
OJASWI DEWANGAN <e6420@IOSs-MacBook-Air.local>
2019-01-21 15:55:23 +0530

    Adding to bitbucket

---
+++ TestIQ/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  TestIQ
+//
+//  Created by OJASWI DEWANGAN on 01/11/18.
+//  Copyright © 2018 OJASWI DEWANGAN. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
